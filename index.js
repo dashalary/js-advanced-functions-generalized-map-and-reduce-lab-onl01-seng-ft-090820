55a7b03e8721d4b3283449c9e289141709a4491f
Dasha Lary <dasha4988@gmail.com>
2020-12-21 17:14:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your functions here
+const map = function(array, func) {
+    let newArray = []
+    for (const element of array) {
+        newArray.push(func(element))
+    }
+    return newArray
+}
+
+const reduce = function(array, func, start) {
+    let i = 1
+    let total
+    if (start) {
+        i = 0
+        total = start
+    } else {
+        total = array[0]
+    }
+    for (i; i < array.length; i++) {
+        total = func(total, array[i])
+    }
+    return total
+}
